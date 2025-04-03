d3f033e36017eade253022f464ccc68952620170
ebraux <emmanuel.braux@imt-atlantique.fr>
2025-04-03 16:05:49 +0200

    test 37

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,7 +1,7 @@
 """
 Scripts module for managing contributions.
 
-test 36
+test 37
 This module contains scripts for:
 - Initializing new contributions (`init.py`)
 - Validating contribution metadata (`validate_contributions.py`)
