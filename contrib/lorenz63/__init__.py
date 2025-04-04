447981e7e68ad2c7df7b21adf1981acab6fed75f
ebraux <emmanuel.braux@imt-atlantique.fr>
2025-04-04 14:39:29 +0200

    test 66

--- contrib/lorenz63/__init__.py
+++ contrib/lorenz63/__init__.py
@@ -1,7 +1,7 @@
 from contrib.lorenz63.models import percent_err
 from hydra.core.config_store import ConfigStore
 
-# test 65
+# test 66
 
 cs = ConfigStore().instance()
 
--- contrib/ose_training/__init__.py
+++ contrib/ose_training/__init__.py
@@ -4,7 +4,7 @@ from hydra.core.config_store import ConfigStore
 import datetime
 import pandas as pd
 
-# test 65
+# test 66
 
 cs = ConfigStore().instance()
 
