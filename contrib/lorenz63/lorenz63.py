e9dba579210bc834786d21f7ad84d81a0df623f7
ebraux <emmanuel.braux@imt-atlantique.fr>
2025-04-10 11:18:25 +0200

    fix pytest action

--- contrib/lorenz63/lorenz63.py
+++ contrib/lorenz63/lorenz63.py
@@ -1,5 +1,10 @@
-"""ocean4dvarnet contribution lorenz63"""
+"""ocean4dvarnet  contribution lorenz63"""
+
 
 def hello_world():
-    """A simple function that returns 'Hello, World!'"""
+    """A simple function that returns 'Hello, World!'
+
+    Returns:
+        str: The string 'Hello, World!'.
+    """
     return "Hello, World!"
